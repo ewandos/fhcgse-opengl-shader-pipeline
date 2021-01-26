81b5588e0cedc4a8ff5223504d34952e4b30ef3a
Philip Ewert <philipewert@icloud.com>
2021-01-26 23:04:59 +0100

    Refactoring
    deleted unnessicary files

--- resources/shader.fs
+++ resources/shader.fs
@@ -19,23 +19,23 @@ uniform Light light;
 uniform sampler2D texture_diffuse;
 uniform sampler2D texture_specular;
 
-void main()
-{
-    // ambient
+void main() {
+    // ambient light
     vec3 ambient = light.ambient * texture(texture_diffuse, TexCoords).rgb;
 
-    // diffuse
+    // diffuse light
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(texture_diffuse, TexCoords).rgb;
 
-    // specular
+    // specular light
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = light.specular * spec * texture(texture_specular, TexCoords).rgb;
 
+    // put it all together
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, alpha);
 }
--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,22 @@
+// PHILIP EWERT - CGSE BIF 5
+// SOURCE: https://learnopengl.com/book/book_pdf.pdf
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <iostream>
+
+// Source
 #include <shader.h>
 #include <camera.h>
 #include <model.h>
 
-#include <iostream>
-
-// SOURCE: https://learnopengl.com/
-
-// callback function when viewport gets resized
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-// input handler
-void processInput(GLFWwindow* window);
-// mouse input handlers
-void mouse_callback(GLFWwindow* window, double xPos, double yPos);
-void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
+// --------------------------
+// VARIABLES
+// --------------------------
 
 // settings
 const unsigned int SCR_WIDTH = 1600;
@@ -47,130 +45,21 @@ glm::vec3 specularIntensity(1.0f, 1.0f, 1.0f);
 glm::vec3 ambientColor = glm::vec3(1.0f, 0.5f, 0.31f);
 glm::vec3 diffuseColor = glm::vec3(1.0f, 0.5f, 0.31f);
 glm::vec3 specularColor = glm::vec3(0.5f, 0.5f, 0.5f);
+
 // transparency
 float alpha = 1.0f;
 
-int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "openGl Ewert", NULL, NULL);
-    if(window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    // correct mismatching window and viewport size
-    glfwSetWindowSize(window, SCR_WIDTH / 2, SCR_HEIGHT / 2);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
-
-    // tell GLFW to capture our mouse
-    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // enable depth test (z-buffer)
-    glEnable(GL_DEPTH_TEST);
-
-    // enable blending
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    // building the shader from the vertex and fragment shader paths
-    Shader shader("resources/shader.vs", "resources/shader.fs");
-
-    // model loading
-    std::vector<Model> models;
-    models.push_back(Model("resources/pizza.obj"));
-    models.push_back(Model("resources/pizza_medium.obj"));
-    models.push_back(Model("resources/pizza_low.obj"));
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    /*
-    relevant transformation matrices
-    1) model = local to world space
-    2) view = world to view space
-    3) projection = view to clip space
-    */
-    // RENDER LOOP
-    while(!glfwWindowShouldClose(window)) {
-
-        // per-frame time logic
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-
-        // input
-        processInput(window);
-
-        // rendering commands here:
-
-        // clear colors:
-        glClearColor(0.85f, 0.85f, 0.85f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // shader needs to be activated before accessing its uniforms
-        shader.use();
-        // lighting
-        shader.setVec3("light.ambient", ambientIntensity);
-        shader.setVec3("light.diffuse", diffuseIntensity);
-        shader.setVec3("light.specular", specularIntensity);
-        shader.setVec3("light.position", lightPos);
-        shader.setVec3("viewPos", camera.Position);
-
-        // set alpha val
-        shader.setFloat("alpha", alpha);
-        // pass cam pos
-
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        shader.setMat4("projection", projection);
-        shader.setMat4("view", view);
-
-        glm::mat4 model = glm::mat4(1.0f);
-        shader.setMat4("model", model);
-
-        float length = glm::length(camera.Position);
-        if(length < 1.5f) {
-            models[0].Draw(shader);
-        }
-        else if(length > 1.5f && length < 3.0f) {
-            models[1].Draw(shader);
-        }
-        else if(length > 3.0f) {
-            models[2].Draw(shader);
-        }
-
-        // swap buffer and poll IO events
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
-}
+// --------------------------
+// FUNCTIONS
+// --------------------------
 
 void processInput(GLFWwindow* window) {
+
+    // Exit Controls
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    // WASD Controls
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -180,7 +69,7 @@ void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 
-    // toggle transparency
+    // toggle transparency with KEY T
     if(glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && !buttonPressed)
         buttonPressed = true;
     if(glfwGetKey(window, GLFW_KEY_T) == GLFW_RELEASE && buttonPressed) {
@@ -193,8 +82,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     if (firstMouse)
     {
         lastX = xpos;
@@ -203,7 +91,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     }
 
     float xOffset = xpos - lastX;
-    float yOffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    // reversed since y-coordinates go from bottom to top
+    float yOffset = lastY - ypos;
 
     lastX = xpos;
     lastY = ypos;
@@ -211,9 +101,112 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     camera.ProcessMouseMovement(xOffset, yOffset);
 }
 
-// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
     camera.ProcessMouseScroll(yoffset);
+}
+
+// --------------------------
+// MAIN-METHOD
+// --------------------------
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__ // for macOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "openGl Ewert", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    // correct mismatching window and viewport size
+    glfwSetWindowSize(window, SCR_WIDTH / 2, SCR_HEIGHT / 2);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // enable depth test (z-buffer)
+    glEnable(GL_DEPTH_TEST);
+
+    // enable blending: handling of transparency
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // building the shader from the vertex and fragment shader paths
+    Shader shader("resources/shader.vs", "resources/shader.fs");
+
+    // model loading
+    std::vector<Model> models;
+    models.push_back(Model("resources/pizza.obj"));
+    models.push_back(Model("resources/pizza_medium.obj"));
+    models.push_back(Model("resources/pizza_low.obj"));
+
+    // RENDER LOOP
+    while(!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime(); // time calculate
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        processInput(window);
+
+        // rendering commands here:
+
+        // clear colors:
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // shader needs to be activated before accessing its uniforms
+        shader.use();
+
+        // lighting
+        shader.setVec3("light.ambient", ambientIntensity);
+        shader.setVec3("light.diffuse", diffuseIntensity);
+        shader.setVec3("light.specular", specularIntensity);
+        shader.setVec3("light.position", lightPos);
+        shader.setVec3("viewPos", camera.Position);
+
+        // set alpha value (binded to controls!)
+        shader.setFloat("alpha", alpha);
+
+        // pass cam pos
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        shader.setMat4("model", model);
+
+        // Level of Detail
+        // --------------------
+        float length = glm::length(camera.Position);
+        if(length < 1.5f) {
+            models[0].Draw(shader);
+        }
+        else if(length > 1.5f && length < 3.0f) {
+            models[1].Draw(shader);
+        }
+        else if(length > 3.0f) {
+            models[2].Draw(shader);
+        }
+
+        // swap buffer and poll IO events
+        // ------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
